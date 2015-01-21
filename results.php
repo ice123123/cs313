b5c5a00fee4c60e7efd1444908240ea76de7a071
ice123123 <hur13013@byui.edu>
2015-01-21 10:20:29 -0700

    testing server

--- results.php
+++ results.php
@@ -8,21 +8,5 @@
 	</head>
 
 	<body>
-	<?php echo "test"; ?>
-<?php
-/*
-	//$lines = file('data.txt');
-	
-	//foreach($lines as $line_num => $line)
-	//{
-		$text = "read test";
-		$myfile = fopen("/OPENSHIFT_DATA_DIR/survey_data.txt", "w");
-		fwrite($myfile, $text);
-		fclose($myfile);
-		$myfile = fopen("/OPENSHIFT_DATA_DIR/survey_data.txt", "w");
-		echo fread($myfile, filesize("/OPENSHIFT_DATA_DIR/survey_data.txt"));
-		fclose($myfile);
-	//}*/
-?>
 	</body>
 </html>
