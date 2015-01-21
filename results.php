c2f65cd30d9c091122ec54724cf6b0425f2dcacf
ice123123 <hur13013@byui.edu>
2015-01-21 10:10:27 -0700

    read/write file test

--- results.php
+++ results.php
@@ -9,12 +9,19 @@
 
 	<body> 
 <?php
-	$lines = file('data.txt');
+
+	//$lines = file('data.txt');
 	
-	foreach($lines as $line_num => $line)
-	{
-		echo "$line";
-	}
+	//foreach($lines as $line_num => $line)
+	//{
+		$text = "read test";
+		$myfile = fopen("/OPENSHIFT_DATA_DIR/survey_data.txt", "w");
+		fwrite($myfile, $text);
+		fclose($myfile);
+		$myfile = fopen("/OPENSHIFT_DATA_DIR/survey_data.txt", "w");
+		echo fread($myfile, filesize("/OPENSHIFT_DATA_DIR/survey_data.txt"));
+		fclose($myfile);
+	//}
 ?>
 	</body>
 </html>
