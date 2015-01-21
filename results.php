010b4ca00f85decdbdc421d9a2288a97e966f2a7
ice123123 <hur13013@byui.edu>
2015-01-21 10:44:16 -0700

    fixed jenkins issue

---
+++ results.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html lang="en-us">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="index.css">
+		
+		<title>Justin Hurley's home page</title>
+	</head>
+
+	<body>
+	<?php echo "test"; ?>
+<?php
+
+	//$lines = file('data.txt');
+	
+	//foreach($lines as $line_num => $line)
+	//{
+		$text = "read test";
+		$myfile = fopen("/OPENSHIFT_DATA_DIR/survey_data.txt", "w");
+		fwrite($myfile, $text);
+		fclose($myfile);
+		$myfile = fopen("/OPENSHIFT_DATA_DIR/survey_data.txt", "w");
+		echo fread($myfile, filesize("/OPENSHIFT_DATA_DIR/survey_data.txt"));
+		fclose($myfile);
+	//}
+?>
+	</body>
+</html>
