bd3a2679c234c5ce65e5e10a062cee8cefc4ded0
ice123123 <hur13013@byui.edu>
2015-01-20 21:04:12 -0700

    upload assignment 2

---
+++ results.php
@@ -0,0 +1,20 @@
+<!DOCTYPE HTML>
+<html lang="en-us">
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="index.css">
+		
+		<title>Justin Hurley's home page</title>
+	</head>
+
+	<body> 
+<?php
+	$lines = file('data.txt');
+	
+	foreach($lines as $line_num => $line)
+	{
+		echo "$line";
+	}
+?>
+	</body>
+</html>
