c65e39c441cf990e5f18728f2594a2e152251ae5
ice123123 <hur13013@byui.edu>
2015-02-11 11:40:21 -0700

    commit

--- homepage.php
+++ homepage.php
@@ -1,4 +1,11 @@
 <?php
+	$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+	$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	
+	echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
+	
 	try
 	{
 		$user = "queryOnly";
