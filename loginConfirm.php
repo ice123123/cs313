4d902833c2745584a8e2a34abc419c4ea4543a2f
ice123123 <hur13013@byui.edu>
2015-02-11 13:59:57 -0700

    update

--- loginConfirm.php
+++ loginConfirm.php
@@ -1,15 +1,35 @@
 <?php
+	$dbHost = "";
+	$dbPort = "";
+	$dbUser = "queryOnly";
+	$dbPassword = "query";
+	$dbName = "game_store";
+	
+	$openShiftVar = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	
+	if ($openShiftVar === null || $openShiftVar == "")
+	{
+		require("setLocalDatabaseCrentials.php");
+	}
+	else
+	{
+		$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+		$dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
+		//$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+		//$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	}
+	
 	try
 	{
-		$user = "queryOnly";
-		$password = "query"; 
-		$db = new PDO("mysql:host=localhost;dbname=game_store", $user, $password);
+		$db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 	}
 	catch (PDOException $ex) 
 	{
 		echo "Error!: " . $ex->getMessage();
 		die(); 
 	}
+	//echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br >\n";
+	
 	$results = $db->query("SELECT username FROM user WHERE username='" . $_POST["username"] . "' AND password='" .  $_POST["password"] . "';");
 	
 	$numRows = $results->rowCount();	
